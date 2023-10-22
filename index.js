d23a15b4bc2a1d987ff81da48609113c71dd93ff
kiyang <109514563+Kiyang101@users.noreply.github.com>
2023-10-23 04:06:42 +0700

    init

---
+++ index.js
@@ -0,0 +1,92 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const Character = require("./models/Character");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.set("strictQuery", false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+//Routes
+
+app.get("/", async (req, res, next) => {
+  try {
+    const characters = await Character.find();
+    res.json(characters);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get("/:name", async (req, res, next) => {
+  try {
+    const character = await Character.findOne({ name: req.params.name });
+    if (!character) {
+      return res.status(404).send("No character found with that name");
+    }
+    res.json(character);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/", async (req, res, next) => {
+  try {
+    const newCharacter = await Character.create(req.body);
+    res.json(newCharacter);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.put("/:name", async (req, res, next) => {
+  try {
+    const updatedCharacter = await Character.findOneAndUpdate(
+      { name: req.params.name },
+      req.body,
+      { new: true }
+    );
+    if (!updatedCharacter) {
+      return res
+        .status(404)
+        .send("No character found with that name to update");
+    }
+    res.json(updatedCharacter);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.delete("/:name", async (req, res, next) => {
+  try {
+    const deletedCharacter = await Character.findOneAndDelete({
+      name: req.params.name,
+    });
+    if (!deletedCharacter) {
+      return res
+        .status(404)
+        .send("No character found with that name to delete");
+    }
+    res.json(deletedCharacter);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
---
+++ models/Character.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const CharacterSchema = new mongoose.Schema({
+    name: String,
+    weapon: String,
+    element: String,
+    rarity: Number,
+    select: Boolean,
+    display: Boolean,
+    updated_at: { type: Date, default: Date.now }
+})
+
+module.exports = mongoose.model('Charcter', CharacterSchema)
