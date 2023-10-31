76ebd7b16c9771be809ebc29437e27a3d0b78a48
kiyang <109514563+Kiyang101@users.noreply.github.com>
2023-10-31 18:36:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,15 @@ const connectDB = async () => {
 
 //Routes
 const blockedIPs = ["127.0.0.1"];
+app.use((req, res, next) => {
+  if (blockedIPs.includes(req.ip)) {
+    res.status(403).send("Access denied!");
+  } else {
+    next();
+  }
+});
 app.get("/", async (req, res, next) => {
   try {
-    if (blockedIPs.includes(req.ip)) {
-      res.status(403).send("Access denied!");
-    } else {
-      next();
-    }
     console.log("Request from IP:", req.ip);
     console.log("User-Agent:", req.headers["user-agent"]);
     console.log("Referer:", req.headers.referer);
