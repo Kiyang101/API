666aef71b7842bf7b7e4ae6b962ea63ea76c7062
kiyang <109514563+Kiyang101@users.noreply.github.com>
2024-02-06 23:27:14 +0700

    .

---
+++ models/Boss.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const BossSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    select: Boolean,
+    updated_at: { type: Date, default: Date.now },
+})
+
+module.exports = mongoose.model("Boss", BossSchema);
